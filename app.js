ed40735787c29ed7467ab7a7bb8f96fb6b0a476b
Swastik Pandey <swastikpandey200513@example.com>
2026-08-21 19:04:09 +0530

    Fix currUser authentication issue

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
 }
-
 
 const express = require("express");
 const app = express();
@@ -18,33 +17,35 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
-const reviewsRouter = require("./routes/review.js")
-const userRouter =  require("./routes/user.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
-main().then(()=>{
-    console.log("connected to Db");
-}).catch( err =>{
-    console.log(err);
-});
+main()
+    .then(() => {
+        console.log("connected to Db");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-async function main(){
+async function main() {
     await mongoose.connect(dbUrl);
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto:{
+    crypto: {
         secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
@@ -67,54 +68,39 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
-
-
-app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     next();
 });
-
-// app.get("/demouser", async(req,res)=>{
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "delta-student"
-//     });
-
-//     let registeredUser = await User.register(fakeUser,"helloworld");
-//     res.send(registeredUser);
-// });
-
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
-
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page not found!"));
 });
 
-app.use((err,req,res,next) => {
-    let {statusCode=500,message="Something went wrong"}= err;
-    res.status(statusCode).render("error.ejs",{message});
+app.use((err, req, res, next) => {
+    let {
+        statusCode = 500,
+        message = "Something went wrong",
+    } = err;
+
+    res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("listening on port 8080");
 });
-
-
